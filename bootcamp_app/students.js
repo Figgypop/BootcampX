e3de6ee85c23adc0262e8341e4498470ff7aa4f7
David Figueroa <figueroadavid93@gmail.com>
2022-11-16 21:01:23 -0500

    parameterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,5 +1,7 @@
 const { Pool } = require('pg');
-const [cohort, limit] = process.argv.slice(2)
+const limit = process.argv.slice[3] || 5;
+const cohort = process.argv[2];
+const values = [`%${cohort}%`, limit];
 
 const pool = new Pool({
   user: 'labber',
@@ -10,10 +12,10 @@ const pool = new Pool({
 
 const sql = `SELECT students.id as student_id, cohorts.name as cohort_name, students.name as student_name 
 FROM students JOIN cohorts ON cohorts.id = cohort_id 
-WHERE cohorts.name LIKE '%${cohort}%'
-LIMIT ${limit || 5}`
+WHERE cohorts.name LIKE $1
+LIMIT $2`
 
-pool.query(sql)
+pool.query(sql, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.student_name} has an id of ${user.student_id} and was in the ${user.cohort_name} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,5 +1,6 @@
 const { Pool } = require('pg');
-const [cohort] = process.argv.slice(2)
+const cohort = process.argv[2];
+const values = [`%${cohort}%`]
 
 const pool = new Pool({
   user: 'labber',
@@ -7,16 +8,16 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
-console.log('cohort:', typeof cohort)
+
 const sql = `SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort_name
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${cohort || 'JUL02'}%'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher`
 
-pool.query(sql)
+pool.query(sql, values)
 .then(res => {
   console.log(res.rows.length)
   res.rows.forEach(row => {
@@ -24,3 +25,5 @@ pool.query(sql)
   })
 })
 .catch(err => console.error('query error', err.stack));
+
+pool.query(sql, values)
